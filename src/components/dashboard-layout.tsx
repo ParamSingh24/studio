c569e6737603eefc862626dc397ec4708952af76
Tech Hero <techhero724@gmail.com>
2025-08-06 16:07:32 +0000

    remove the cause of this error

--- src/components/dashboard-layout.tsx
+++ src/components/dashboard-layout.tsx
@@ -31,7 +31,7 @@ export default function DashboardLayout({
           <SidebarMenu>
             {menuItems.map((item) => (
               <SidebarMenuItem key={item.href}>
-                <Link href={item.href}>
+                <Link href={item.href} legacyBehavior passHref>
                   <SidebarMenuButton isActive={pathname === item.href}>
                     <item.icon className="size-4" />
                     <span>{item.label}</span>
